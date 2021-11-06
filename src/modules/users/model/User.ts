73e9106547416d2f0c6518f0abe498deb6cd3bc3
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-11-06 09:49:40 -0300

    done

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,22 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+
+    if (!this.admin) {
+      this.admin = false;
+    }
+  }
 }
 
 export { User };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (user === undefined) {
+      throw new Error("User not found");
+    }
+
+    if (user.admin === false) {
+      throw new Error("User is not admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
